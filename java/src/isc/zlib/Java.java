0e6ac0f4955594e1859fc9abae2f6e8d9cacc0c8
eduard93 <lebeduk93@gmail.com>
2018-12-29 16:57:23 +0300

    Java implementation

---
+++ java/src/isc/zlib/Java.java
@@ -0,0 +1,30 @@
+package isc.zlib;
+
+import java.util.Arrays;
+import java.util.zip.Deflater;
+
+public abstract class Java {
+
+    public static byte[] compress(String inputString) {
+        byte[] output = new byte[100];
+        try {
+            // Encode a String into bytes
+            byte[] input = inputString.getBytes("UTF-8");
+
+            // Compress the bytes
+
+            Deflater compresser = new Deflater();
+            compresser.setInput(input);
+            compresser.finish();
+            int compressedDataLength = compresser.deflate(output);
+            compresser.end();
+            output = Arrays.copyOfRange(output, 0, compressedDataLength);
+
+        } catch (java.io.UnsupportedEncodingException ex) {
+            // handle
+        }
+
+
+        return output;
+    }
+}
