7120f3622b23f488fa28549eaa67717146e3eb93
eduard93 <lebeduk93@gmail.com>
2018-12-29 21:18:04 +0300

    Add -O2 to gcc

--- c/build.bat
+++ c/build.bat
@@ -2,7 +2,7 @@ set WINBUILDS_HOME=C:\WinBuilds
 set GLOBALS_HOME=C:\InterSystems\Ensemble
 set PATH=%WINBUILDS_HOME%\bin;%PATH%
 
-gcc -shared -I%GLOBALS_HOME%\dev\cpp\include zlibisc.c -lz -o zlibisc.dll
+gcc -O2 -shared -I%GLOBALS_HOME%\dev\cpp\include zlibisc.c -lz -o zlibisc.dll
 cp zlibisc.dll %GLOBALS_HOME%\bin
 cp %WINBUILDS_HOME%\bin\libz-1.dll %GLOBALS_HOME%\bin
 
